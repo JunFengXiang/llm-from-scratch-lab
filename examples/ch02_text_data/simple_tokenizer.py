54dc7665ac3b854dd63643fb15068ab79e66fdd6
JunFengXiang <xjf147258@qq.com>
2026-08-10 22:38:23 +0800

    补充简单分词器实现

---
+++ examples/ch02_text_data/simple_tokenizer.py
@@ -0,0 +1,35 @@
+import re
+
+class SimpleTokenizerV1:
+    def __init__(self,vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {
+            integer: token
+            for token,integer in vocab.items()
+        }
+    def encode(self,text):
+        preprocessed = re.split(
+            r'([,.?_!"()\']|--|\s)',
+            text
+        )#按照原始规则分词
+        preprocessed = [
+            item.strip()
+            for item in preprocessed
+            if item.strip()
+        ]#删除空白项
+        ids = [
+            self.str_to_int[token]
+            for token in preprocessed
+        ]#用str_to_int查询每个词元
+        return ids#返回id列表
+    def decode(self,ids):
+        text = " ".join(
+            self.int_to_str[token_id]
+            for token_id in ids
+        )
+        text = re.sub(
+            r'\s+([,.?!"()\'])',
+            r'\1',
+            text,
+        )
+        return text
