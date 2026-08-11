451e9a58defcd03ef169c86441eb150618b5dbb7
JunFengXiang <xjf147258@qq.com>
2026-08-11 17:59:45 +0800

    simplify verdict tokenization

--- examples/ch02_text_data/tokenize_verdict.py
+++ examples/ch02_text_data/tokenize_verdict.py
@@ -1,8 +1,4 @@
-"""import re
-text = "Hello ,world. This, is a test."
-result = re.split(r"([,.]|\s)",text)
-result = [item for item in result if item.strip()]
-print(result)"""
+
 #完整版示例
 from simple_tokenizer import SimpleTokenizerV1
 import re
